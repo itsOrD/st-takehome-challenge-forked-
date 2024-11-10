aad5690ad2ed2cbb454dba7d0e0be07d9b40e11d
Matthew Beckerleg <matthew.beckerleg+github@gmail.com>
2024-11-10 15:07:58 -0800

    POC for single article view with hardcoded article obj

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
-import Article from "./components/Article";
+import ArticleCard from "./components/ArticleCard/ArticleCard";
 
 function App() {
+
+  const testArticle = {
+    "articleId": 22,
+    "title": "Article One",
+    "articleURL": "https://example.com/1",
+    "thumbnailURL": "https://dummyimage.com/780x500/CB6015/E8DDCB",
+    "articleType": "Article",
+    "primarySection": "Politics",
+    "author": "Sarah Doe",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu lacus vel sem egestas efficitur. Mauris feugiat ex dui, et mattis nibh tempor id."
+  }
+
   return (
     <div>
-      <Article />
+      <ArticleCard article={testArticle}/>
     </div>
   );
 }
---
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,34 @@
+import styles from './ArticleCard.module.css';
+
+interface Article {
+  articleId: number;
+  title: string;
+  body: string;
+  thumbnailURL: string;
+  articleURL: string;
+  // ToDo: add other fields as needed
+}
+
+interface ArticleCardProps {
+  article: Article;
+}
+
+const ArticleCard = ({ article }: ArticleCardProps) => {
+  // ToDo: add business logic as req'd for preview vs full article
+  
+  return (
+    <article className={styles.card}>
+      <div className={styles.imageContainer}>
+        <img
+          src={article.thumbnailURL}
+          alt={article.title}
+        />
+      </div>
+      <h2>{article.title}</h2>
+      <p>{article.body}</p>
+      <a href={article.articleURL}>Read More</a>
+    </article>
+  );
+};
+
+export default ArticleCard;
