6d9ba7e4d2a1c3eff47ac4ea639c11a008a28999
Matthew Beckerleg <matthew.beckerleg+github@gmail.com>
2024-11-10 02:05:04 +0000

    Setup for local dev

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,10 +1,10 @@
 const Article = () => {
-
-    return (
-        <div>
-            hi
-        </div>
-    )
+  return (
+    <div>
+      hi, this is a test to see if I have to use codesandbox or if i can pull
+      local and then push back to sandbox for submission
+    </div>
+  );
 };
 
 export default Article;
