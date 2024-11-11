c894283eef2f88f81248633c7e2c6ea39a3fe558
Matthew Beckerleg <matthew.beckerleg+github@gmail.com>
2024-11-10 17:17:17 -0800

    Establishes vitest test bed for unit tests (and simple integration tests) (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+/// <referenc types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import type { UserConfig } from "vite";
+import type { InlineConfig } from "vite";
 
-// https://vitejs.dev/config/
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig
+}
+
+
 export default defineConfig({
   plugins: [react()],
-});
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setup.test.ts',
+  }
+} as VitestConfigExport);
